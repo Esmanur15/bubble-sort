17532156d6da18f391f09ce291b99c042d53ca69
Esmanur15 <esmanurdursun.megayazilim@outlook.com>
2021-09-22 09:48:46 +0300

    ilk commit

---
+++ BubleSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BubleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = { 6, 12, 24, 3, 8, 4};
+            kabarcikSirala(dizi);
+            diziYazdir(dizi);
+        }
+        public static void kabarcikSirala(int[]siralanacakDizi) {
+ 
+            int i = 1,  j, deger;
+            int diziAdet = siralanacakDizi.Length;
+            while (i < diziAdet)
+            {
+                j = diziAdet - 1;
+                while (j >= 1)
+                {
+                    if (siralanacakDizi[j - 1] > siralanacakDizi[j])
+                    {
+                        deger = siralanacakDizi[j];
+                        siralanacakDizi[j] = siralanacakDizi[j - 1];
+                        siralanacakDizi[j - 1] = deger;
+                    }
+                    j--;
+                }
+                i++;
+            }
+        }
+ 
+        public static void diziYazdir(int[] dizi) {
+ 
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.WriteLine(dizi[i]);
+            }
+            Console.ReadKey();
+    }
+}
+}
